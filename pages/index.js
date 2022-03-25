26f2547839b1f0662663fcee83c24a65bdb90927
tezosmiami <94186639+tezosmiami@users.noreply.github.com>
2022-03-25 16:19:44 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-Loimport Head from 'next/head'
+import Head from 'next/head'
 import Image from 'next/image'
 import {useState, useEffect} from 'react'
 import { usePassengerContext } from "../context/passenger-context";
